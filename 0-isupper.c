2e9265897372937c9acf630fa416b616636c7f34
Khaled22ramadan <ramdankhaled09@gmail.com>
2023-02-24 17:29:51 -0800

    Write a function that checks for uppercase character.

---
+++ 0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -  checks for uppercase character.
+ * @x: the number to be checked
+ * Return: 1 for upper letter 0 for else
+ */
+int _isupper(int c)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+return (0);
+}
